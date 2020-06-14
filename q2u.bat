d6fc8aa87fd7760e72192da9fb137ef768583d9f
rozzelle <rozzellegage@gmail.com>
2020-06-14 12:26:39 -0400

    Making script more robust by checking for devcon installation before running script

--- q2u.bat
+++ q2u.bat
@@ -1,5 +1,28 @@
 @ECHO off
-ECHO.
+
+:: Check command line arguments
+IF NOT "%~1"=="" IF /I NOT "%~1"=="/A" GOTO Syntax
+ 
+:: Check if DEVCON.EXE is available and if not, offer to download it
+SET DevconAvailable=
+SET Download=
+DEVCON.EXE /? >NUL 2>&1
+IF ERRORLEVEL 1 (
+	SET DevconAvailable=No
+	ECHO This batch file requires Microsoft's DEVCON untility.
+	SET /P Download=Do you want to download it now? [y/N] 
+)
+ 
+:: Start download if requested
+IF /I "%Download%"=="Y" (
+	START "DevCon" "https://superuser.com/questions/1002950/quick-method-to-install-devcon-exe"
+	ECHO.
+	ECHO Install the downloaded file and make sure DEVCON.EXE is in the PATH.
+	ECHO Then try again.
+)
+ 
+:: Abort if DEVCON.EXE is not available yet
+IF "%DevconAvailable%"=="No" GOTO End
 
 SETLOCAL
 SET Q2U=
